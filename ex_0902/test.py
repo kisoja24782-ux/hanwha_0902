248a6a0ed842bb6095b299eca76481b0d04ee0ee
kisoja24782-ux <kisoja24782@gmail.com>
2026-09-04 17:41:16 +0900

    python grammar

---
+++ ex_0902/test.py
@@ -0,0 +1,45 @@
+
+
+# print("AI 서비스 백엔드 프로그래밍 실무")
+# print("==================")
+# print("파이썬 기본 문법, 시간 : 8")
+# print("클래스, 시간 : 8")
+# print("데코레이터, 시간 : 8")
+# print("예외 처리, 시간 : 8")
+# print("로깅, 시간 : 8")
+
+title = "AI 서비스 백엔드 프로그래밍 실무"
+linecut = "="
+grammar = "파이썬 기본 문법"
+klass = "클래스"
+deco = "데코레이터"
+ex = "예외처리"
+log = "로깅"
+time = "시간"
+number = "8"
+
+print(title)
+print(linecut*40)
+# print(grammar,",", h,":",i)
+# print(klass,",", h,":",i)
+# print(deco,",", h,":",i)
+# print(ex,",", h,":",i)
+# print(log,",", h,":",i)
+
+
+AIlist = ["파이썬 기본 문법","클래스","데코레이터","예외처리","로깅"]
+
+# print(AIlist[0],",", time,":",number)
+# print(AIlist[1],",", time,":",number)
+# print(AIlist[2],",", time,":",number)
+# print(AIlist[3],",", time,":",number)
+# print(AIlist[4],",", time,":",number)
+
+for x in AIlist:
+    print(x,",",time,":",number)
+
+
+
+for y in range(10):
+    print(y,end=" ")
+
